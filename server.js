7c2cd9de48071c91025578202b3a334d8048678d
chungchau92 <chungchau92@gmail.com>
2023-01-04 22:09:50 +0700

    add schema file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const routes = require("./controllers")
+
 const app = express();
 
 const POST = process.env.POST || 3001;
 
+app.use(routes);
+
 app.listen(POST, () => {
-    console.log("Listening on http://localhost"+ POST);
+    console.log("Listening on http://localhost:"+ POST);
 });

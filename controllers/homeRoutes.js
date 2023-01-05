ad29d5272c0564cfdde1abefdada62b322bf06df
chungchau <chungchau92@gmail.com>
2023-01-05 17:35:19 +0700

    add handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req,res) => {
-    res.end("foo")
+    res.render("homepage")
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const exphbs = require("express-handlebars")
+
 const routes = require("./controllers")
 
 const app = express();
@@ -7,6 +9,10 @@ const app = express();
 const POST = process.env.POST || 3001;
 
 app.use(routes);
+
+// Inform Express.js on which template engine to use
+app.engine("handlebars", exphbs.engine())
+app.set('view engine', 'handlebars');
 
 app.listen(POST, () => {
     console.log("Listening on http://localhost:"+ POST);

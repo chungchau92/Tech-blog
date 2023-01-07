e8a01d68659de91ee1bcd021451d4e20ed4c01aa
chungchau <chungchau92@gmail.com>
2023-01-07 12:11:57 +0700

    still fix bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
-const { User } = require('../../models');
-
+const { User } = require("../../models");
 
 router.post("/signup", async (req, res) => {
     try {
+        console.log(123)
+
         const userData = await User.create(req.body);
 
         res.status(200).json(userData);
+
     } catch (err) {
         res.status(400).json(err);
     }
 })
-
 
 router.get("/signup", async (req,res) => {
     const userData = await User.findAll()

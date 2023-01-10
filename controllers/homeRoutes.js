5d2f4561d03f13c019098cdcfca865f2e867e660
chungchau <chungchau92@gmail.com>
2023-01-10 15:41:46 +0700

    commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const auth = require("../utils/auth")
 
 router.get("/", (req,res) => {
     res.render("homepage",{logged_in: req.session.logged_in})
@@ -12,8 +13,8 @@ router.get("/signUp", (req,res) => {
     res.render("signUp")
 })
 
-router.get("/dashbroad", (req,res) => {
-    res.render("dashbroad")
+router.get("/dashbroad", auth, (req,res) => {
+    res.render("dashbroad", {logged_in: req.session.logged_in});
 })
 
 module.exports = router;

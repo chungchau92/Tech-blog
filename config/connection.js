2ca493b1be255473b051ca9bba505d6cd0344bda
chungchau92 <chungchau92@gmail.com>
2023-01-23 23:12:32 +0700

    fix heroku issues

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
+if (process.env.CLEARDB_DATABASE_URL) {
   sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
   sequelize = new Sequelize(
